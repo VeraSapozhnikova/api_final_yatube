f8aeb79640c9b4c8acfa1a3b776606d78b45bde5
Vera <121445155+VeraSapozhnikova@users.noreply.github.com>
2023-04-23 14:27:41 +0300

    1st commit
    1st commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# api_final
-api final
+Описание проекта:
+
+Проект api_yatube - это API социальной сети yatube.
+
+С помощью api_yatube можно запрашивать данные о постах, группах, комментариях в социальной сети Yatube, а также создавать новые.
+
+Yatube - это учебный проект курса Python-разработчик от Яндекс Практикума.
+
+Автор: Вера Сапожникова
+
+Запуск проекта:
+
+Клонировать репозиторий: git clone git@github.com:VeraSapozhnikova/api_final_yatube.git
+Cоздать виртуальное окружение: python -m venv venv
+Активировать виртуальное окружение: . venv/Scripts/activate
+Установить зависимости: pip install -r requirements.txt
+Выполнить миграции: python manage.py migrate
+Запустить проект: python manage.py runserver
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,59 @@
-# TODO:  Напишите свой вариант
+from django.shortcuts import get_object_or_404
+from rest_framework import pagination, permissions, viewsets, mixins
+from rest_framework.filters import SearchFilter
+
+from posts.models import Follow, Group, Post, User
+from .permissions import OwnerOrReadOnly
+from .serializers import (CommentSerializer, FollowSerializer, GroupSerializer,
+                          PostSerializer, UserSerializer)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (OwnerOrReadOnly,)
+    pagination_class = pagination.LimitOffsetPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (OwnerOrReadOnly,)
+
+    def perform_create(self, serializer):
+        post = get_object_or_404(Post, pk=self.kwargs['post_id'])
+        serializer.save(author=self.request.user, post=post)
+
+    def get_queryset(self):
+        post = get_object_or_404(Post, pk=self.kwargs['post_id'])
+        return post.comments.all()
+
+
+class FollowViewSet(mixins.CreateModelMixin,
+                    mixins.ListModelMixin,
+                    viewsets.GenericViewSet):
+    queryset = Follow.objects.all()
+    serializer_class = FollowSerializer
+    pagination_class = pagination.LimitOffsetPagination
+    permission_classes = (permissions.IsAuthenticated,)
+    filter_backends = (SearchFilter,)
+    search_fields = ('following__username', 'user__username',)
+
+    def get_queryset(self):
+        """Возвращает все подписки пользователя, сделавшего запрос."""
+        return Follow.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
